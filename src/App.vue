d99745cb1140f0d5f0835b2921b465423876c8ee
Mikhail Metrikin <metrikin@gmail.com>
2019-07-04 21:28:21 -0700

    Binding in properties

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <TestComponent msg="TESING ONE TIME BIND" rawHtml="<h1>This h1 is intrepreted HTML</h1>" />
+    <TestComponent 
+      id="passed-id"
+      myClass="passed-class"
+      msg="TESING ONE TIME BIND" 
+      rawHtml="<h1>This h1 is intrepreted HTML</h1>" 
+      />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="test-root">
-    <p>Testing this new component rendering</p>
+  <div v-bind:id=id>
+    <p v-bind:class=myClass >Testing this new component rendering</p>
     <p v-once>My one time bind message is {{ msg }}</p>
+    <!-- Raw HTML directive for interpreting HTML -->
     <div v-html=rawHtml></div>
   </div>
 </template>
@@ -11,6 +12,8 @@
 export default {
   name: 'TestComponent',
   props: {
+    myClass: String,
+    id: String,
     msg: String,
     rawHtml: String,
   }
@@ -18,6 +21,13 @@ export default {
 </script>
 
 <style scoped>
+  #passed-id {
+    background-color: red;
+  }
 
+  .passed-class {
+    font-style: italic;
+    font-weight: bold;
+  }
 </style>
 
