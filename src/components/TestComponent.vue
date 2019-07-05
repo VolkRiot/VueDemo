2868a4c5eec513f1d8e93a4037f6e77081259223
Mikhail Metrikin <metrikin@gmail.com>
2019-07-04 21:48:11 -0700

    More examples of Vue functionality

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,33 +1,41 @@
 <template>
-  <div v-bind:id=id>
-    <p v-bind:class=myClass >Testing this new component rendering</p>
+  <div v-once v-bind:id="id">
+    <p v-once v-bind:class="myClass">Testing this new component rendering</p>
     <p v-once>My one time bind message is {{ msg }}</p>
     <!-- Raw HTML directive for interpreting HTML -->
-    <div v-html=rawHtml></div>
+    <div v-html="rawHtml"></div>
+    <button v-bind:disabled="isDisabled">Test Button</button>
+    <p>{{ reverseMessage.split('').reverse().join('') }}</p>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'TestComponent',
+  name: "TestComponent",
   props: {
     myClass: String,
     id: String,
     msg: String,
-    rawHtml: String,
+    rawHtml: String
+  },
+  data: function() {
+    return {
+      isDisabled: true,
+      reverseMessage: 'This message can be reversed using JS in mustache syntax',
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-  #passed-id {
-    background-color: red;
-  }
+#passed-id {
+  background-color: red;
+}
 
-  .passed-class {
-    font-style: italic;
-    font-weight: bold;
-  }
+.passed-class {
+  font-style: italic;
+  font-weight: bold;
+}
 </style>
 
