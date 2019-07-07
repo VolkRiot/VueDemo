2fdffd8f8c2183147de31f8922d165a88549fe33
Mikhail Metrikin <metrikin@gmail.com>
2019-07-07 13:00:15 -0700

    Learned a lesson about v-once

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <button v-on:click="toggleTest">Toggle</button>
+    <p v-if="test">Value of test is true</p>
+    <p v-else>Value of test is false</p>
     <TestComponent 
       id="passed-id"
       myClass="passed-class"
       msg="TESING ONE TIME BIND" 
       rawHtml="<h1>This h1 is intrepreted HTML</h1>" 
+      count="0"
       />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
@@ -20,6 +24,16 @@ export default {
   name: 'app',
   components: {
     TestComponent
+  },
+  data() {
+    return {
+      test: true
+    }
+  },
+  methods: {
+    toggleTest() {
+      this.test = !this.test;
+    },
   }
 }
 </script>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,15 +1,20 @@
 <template>
-  <div v-once v-bind:id="id">
+  <div v-bind:id="id">
     <p v-once v-bind:class="myClass">Testing this new component rendering</p>
-    <p v-once>My one time bind message is {{ msg }}</p>
+    <p>My one time bind message is {{ msg }}</p>
     <!-- Raw HTML directive for interpreting HTML -->
     <div v-html="rawHtml"></div>
-    <button v-bind:disabled="isDisabled">Test Button</button>
-    <p>{{ reverseMessage.split('').reverse().join('') }}</p>
-    <p v-if='ifDirectiveActive'>This text is shown because directive is set to {{ ifDirectiveActive }}</p>
-    <a target="_blank" v-bind:href="urlBindingExample">This is an example of a link with a bound property</a>
+    <button v-on:click="toggleReversed">Test Button</button>
+    <div v-if='makeReverse'>{{ reverseMessage.split('').reverse().join('') }}</div>
+    <div v-else>{{ reverseMessage }}</div>
+    <p v-if="ifDirectiveActive">This text is shown because directive is set to {{ ifDirectiveActive }}</p>
+    <a target="_blank" :href="urlBindingExample">This is an example of a link with a bound property</a>
     <p>The following is an example of he v-on initiative</p>
-    <button v-on:[onAttribute]="clickDemo">Click me!</button>
+    <button v-on:[onAttribute]="increment">Click to increment count!</button>
+    <p>{{`Counter: ${count}`}}</p>
+    <form v-on:submit.prevent="onSubmit">
+      <input type="text" />
+    </form>
   </div>
 </template>
 
@@ -23,18 +28,30 @@ export default {
     msg: String,
     rawHtml: String
   },
-  methods: {
-    clickDemo: function() { console.log('Click detected'); }
-  },
   data: function() {
     return {
-      isDisabled: true,
-      onAttribute: 'click',
+      isDisabled: false,
+      onAttribute: "click",
       ifDirectiveActive: true,
-      urlBindingExample: 'http://www.google.com',
-      reverseMessage: 'This message can be reversed using JS in mustache syntax',
+      urlBindingExample: "http://www.google.com",
+      reverseMessage:
+        "This message can be reversed using JS in mustache syntax",
+      makeReverse: true,
+      count: 0,
     };
-  }
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    onSubmit() {
+      // console.log("Submit called");
+    },
+    toggleReversed() {
+      this.makeReverse = !this.makeReverse;
+    }
+  },
+
 };
 </script>
 
